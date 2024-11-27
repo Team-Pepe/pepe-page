fe27133c5a98f9dffeb11b86b02e01f232e58e58
schnneider <schnneider.arias@utp.edu.co>
2024-11-26 19:15:28 -0500

    nueva funcion menu lateral

--- assets/js/menu_lateral.js
+++ assets/js/menu_lateral.js
@@ -7,3 +7,12 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
+
+function toggleSubMenu() {
+    var subMenu = document.getElementById("subMenu");
+    if (subMenu.style.display === "block") {
+        subMenu.style.display = "none";
+    } else {
+        subMenu.style.display = "block";
+    }
+}
