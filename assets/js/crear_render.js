56ce0a144f72d88b23668cbebd9c46752cefa6a1
schnneider <schnneider.arias@utp.edu.co>
2024-12-03 01:09:39 -0500

    ajuste a menu lateral

--- assets/js/crear_render.js
+++ assets/js/crear_render.js
@@ -1,15 +1,3 @@
-// Función para abrir el menú lateral
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-// Función para cerrar el menú lateral
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
-
 // Guardar tareas al enviar el formulario
 const taskForm = document.querySelector('#taskForm');
 const taskList = JSON.parse(localStorage.getItem('taskList')) || [];
